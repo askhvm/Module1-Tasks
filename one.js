8936bc5789287743029e505c79041639101243dc
Maga <ashamahov.itrum@gmail.com>
2022-12-22 15:09:15 +0300

    fix task1

--- one.js
+++ one.js
@@ -1,7 +1,11 @@
-let arr = [{ name: "Milk", price: 20, amount: 15 },
-{ name: "Coffee", price: 30, amount: 17 },
-{ name: "Tea", price: 10, amount: 14 }];
+const arr = [
+  { name: "Milk", price: 20, amount: 15 },
+  { name: "Coffee", price: 30, amount: 17 },
+  { name: "Tea", price: 10, amount: 14 },
+];
 
-let result = arr.map(obj => ({name: obj.name, 
-    total: obj.price * obj.amount}));
+const result = arr.map((obj) => ({
+  name: obj.name,
+  total: obj.price * obj.amount,
+}));
 console.log(result);
