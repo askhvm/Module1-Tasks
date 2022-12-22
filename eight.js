f2f3efab935f8fdb26e90c5a35a4089759001c84
Maga <ashamahov.itrum@gmail.com>
2022-12-22 15:48:55 +0300

    task8 fixed

--- eight.js
+++ eight.js
@@ -1,7 +1,7 @@
     let arr = [{ name: "Ivan", age: 24 }, { name: "Oleg", age: 16}, { name: "Igor", age: 24}]
 
     const sum = (array) => {
-        return result = array.reduce((acc,e) => acc + e.age ,0)
+        return result = array.reduce((sumAge,e) => sumAge + e.age ,0)
     }
 
     console.log(sum(arr))
