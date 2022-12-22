d7af9490e4e9d728627db6e120497ffdab5be876
Maga <ashamahov.itrum@gmail.com>
2022-12-22 13:59:51 +0300

    task13 done

---
+++ thirteen.js
@@ -0,0 +1,25 @@
+let arr = ["str", 123, "123", true, null, {}, 2];
+
+const response = (array) => {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    switch (typeof array[i]) {
+      case "string":
+        array[i] = "Это строка";
+        break;
+      case "number":
+        array[i] = "Это число";
+        break;
+      case "boolean":
+        array[i] = "Это булево значение";
+        break;
+      default:
+        array[i] = "Неизвестное значение";
+    }
+  }
+
+  return array.join('\n');
+};
+
+console.log(response(arr));
