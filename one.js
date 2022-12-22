10ec537b2419a19b5a0a3efb2b9b6243ac7b676d
Maga <ashamahov.itrum@gmail.com>
2022-12-22 08:53:10 +0300

    task1 done

---
+++ one.js
@@ -0,0 +1,7 @@
+let arr = [{ name: "Milk", price: 20, amount: 15 },
+{ name: "Coffee", price: 30, amount: 17 },
+{ name: "Tea", price: 10, amount: 14 }];
+
+let result = arr.map(obj => ({name: obj.name, 
+    total: obj.price * obj.amount}));
+console.log(result);
