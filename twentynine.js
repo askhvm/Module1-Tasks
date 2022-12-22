ae925825ceaeb171334957aec29c28066c33bad1
Maga <ashamahov.itrum@gmail.com>
2022-12-22 10:33:56 +0300

    task29 done

---
+++ twentynine.js
@@ -0,0 +1,12 @@
+let user = {
+    id: 123,
+    first_name: "Ivan",
+    last_name: "Ivanov",
+    password: "new password",
+    age: 15
+}
+
+const {id,first_name: firstName, last_name: lastName, ...rest} = user
+
+console.log(firstName,lastName)
+
