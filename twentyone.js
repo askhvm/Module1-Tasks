adbae8136e1f603edaf29dbe10df18562a743d34
Maga <ashamahov.itrum@gmail.com>
2022-12-22 16:43:09 +0300

    task21 done

---
+++ twentyone.js
@@ -0,0 +1,8 @@
+let user = { first_name: "Ivan", last_name: "Ivanov", email: "ivanov@pochta.com" }
+
+const emailConcater = (obj) => {
+    return `${obj.last_name} ${obj.first_name} ${obj.email}`
+}
+
+
+console.log(emailConcater(user))
