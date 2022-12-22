5afc1c12498644da9e7da240ca28d2a21ee48329
Maga <ashamahov.itrum@gmail.com>
2022-12-22 16:26:36 +0300

    task18 done

---
+++ eighteen.js
@@ -0,0 +1,13 @@
+let name = "Иванов Пётр Андреевич"
+
+const objected = (fullName) => {
+    let eachOfName = fullName.split(' ')
+    const obj = {
+        first_name: eachOfName[1],
+        last_name: eachOfName[0],
+        patronymic_name: eachOfName[2]
+    }
+    return obj
+}
+
+console.log(objected(name))
