abbc7058f8114099462ba7cac82cf7fb59c8524f
Maga <ashamahov.itrum@gmail.com>
2022-12-23 09:29:38 +0300

    task12 done

---
+++ twelve.js
@@ -0,0 +1,7 @@
+const arr = Array(10);
+let result = "";
+for (let i = 0; i < 10; i++) {
+  arr[i] = [...Array(10)].map((_, j) => ("" + i + j) ** 2);
+  result += arr[i].join(" ") + "\n";
+}
+console.log(result);
