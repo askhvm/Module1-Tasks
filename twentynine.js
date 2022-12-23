22675d921f7934876455c25ca5655ea5b9b778a1
Maga <ashamahov.itrum@gmail.com>
2022-12-23 11:19:47 +0300

    task29 edited

--- twentynine.js
+++ twentynine.js
@@ -1,12 +1,11 @@
-let user = {
-    id: 123,
-    first_name: "Ivan",
-    last_name: "Ivanov",
-    password: "new password",
-    age: 15
-}
+const user = {
+  id: 123,
+  first_name: "Ivan",
+  last_name: "Ivanov",
+  password: "new password",
+  age: 15,
+};
 
-const {id,first_name: firstName, last_name: lastName, ...rest} = user
+const { id, first_name: firstName, last_name: lastName, ...rest } = user;
 
-console.log(firstName,lastName)
-
+console.log(firstName, lastName);
