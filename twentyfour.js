1a4ae671c1e5cef5345902e9949b6dba3aefbda6
Maga <ashamahov.itrum@gmail.com>
2022-12-23 11:21:53 +0300

    task24 edited

--- twentyfour.js
+++ twentyfour.js
@@ -1,10 +1,10 @@
-let arr = [4, 5, 2, 4, 1, 5, 3]
+const arr = [4, 5, 2, 4, 1, 5, 3];
 
-function sorting(array,str) {
-    if (str === 'ASC') array.sort((a,b) => a - b)
-    if (str === 'DESC') array.sort((a,b) => b - a)
+function sorting(array, str) {
+  if (str === "ASC") array.sort((a, b) => a - b);
+  if (str === "DESC") array.sort((a, b) => b - a);
 
-    return array
+  return array;
 }
 
-console.log(sorting(arr,'ASC'))
+console.log(sorting(arr, "ASC"));
