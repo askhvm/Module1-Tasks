928e513d2cd80065d4a731842c1997b52436f353
Maga <ashamahov.itrum@gmail.com>
2022-12-23 11:16:44 +0300

    task23 edited

--- twentythree.js
+++ twentythree.js
@@ -1,21 +1,23 @@
-let arr = ["aaa", "bbb", "ccc", "aaa", "bbb", "aaa"]
+const arr = ["aaa", "bbb", "ccc", "aaa", "bbb", "aaa"];
 
 function unique(array) {
-    let unic = array.filter((e, i, ar) => ar.indexOf(e) === i)
+  const unic = array.filter((e, i, ar) => ar.indexOf(e) === i);
 
-    let amount = array.reduce((acc, val) =>
-        (acc[val] = (acc[val] || 0) + 1, acc), {})
-    
-    let vals = Object.values(amount)
-    let result = []
+  const amount = array.reduce(
+    (acc, val) => ((acc[val] = (acc[val] || 0) + 1), acc),
+    {}
+  );
 
-    for (let i = 0; i < unic.length; i++) {
-        result[i] = unic[i] + ' ' + vals[i]
-    }
-    
-    return result.join('\n')
+  const vals = Object.values(amount);
+  const result = [];
+
+  for (let i = 0; i < unic.length; i++) {
+    result[i] = unic[i] + " " + vals[i];
+  }
+
+  return result.join("\n");
 }
 
-console.log(unique(arr))
+console.log(unique(arr));
 
 
