8c9df2326b1cf78b539d079c8e41cac7cb8912b4
Maga <ashamahov.itrum@gmail.com>
2022-12-23 10:44:46 +0300

    nine edited

--- nine.js
+++ nine.js
@@ -1,10 +1,20 @@
-    let arr = [{ name: "Ivan", age: 24 }, { name: "Oleg", age: 16}, { name: "Igor", age: 24}]
+let arr = [
+  { name: "Ivan", age: 24 },
+  { name: "Oleg", age: 16 },
+  { name: "Igor", age: 24 },
+];
 
-    const range = (array) => {
-        let min = Math.min(...array.map(e => e.age))
-        let max = Math.max(...array.map(e => e.age))
-        
-        return {min, max}
+const range = (array) => {
+  let ages = array.map((user) => user.age);
+  for (let i = 0; i <= ages.length; i++) {
+    if (ages[i] > ages[i + 1]) {
+      max = ages[i];
+      min = ages[i + 1];
+    } else if (ages[i] < ages[i + 1]) {
+      min = ages[i];
+      max = ages[i + 1];
     }
-
-    console.log(range(arr))
+  }
+  return { min, max };
+};
+console.log(range(arr));
