1daf73b87a980183cb3c91fdf7d202642567fd39
Maga <ashamahov.itrum@gmail.com>
2022-12-23 11:12:03 +0300

    task15 edite

--- fifteen.js
+++ fifteen.js
@@ -1,12 +1,11 @@
 let arr = [1, 2, 3, 4];
 
 const filt = (array) => {
-  let result;
-  result = array.filter((e) => e % 2 == 0);
+  const result = array.filter((num) => num % 2 == 0);
   return result;
 };
 
-const mult = (array) => array.map((e) => e * 2);
+const mult = (array) => array.map((digit) => digit * 2);
 
 const transform = (array, filt, mult) => {
   return mult(filt(array)).join("\n");
