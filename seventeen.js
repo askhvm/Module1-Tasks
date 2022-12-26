58f16ad64a9f11eb5f44e5ec46036fc30d352097
Maga <ashamahov.itrum@gmail.com>
2022-12-26 12:33:38 +0300

    task17 edited

--- seventeen.js
+++ seventeen.js
@@ -1,10 +1,10 @@
-let arr = [1, 2, 3];
-let arr2 = [4, 5];
-let arr3 = [6, 7];
+const arr = [1, 2, 3];
+const arr2 = [4, 5];
+const arr3 = [6, 7];
 
 function arrConcate(first, second, third) {
-  let empty = [];
-  let final = empty.concat(...arguments)
+  const empty = [];
+  const final = empty.concat(...arguments)
   return final;
 }
 
